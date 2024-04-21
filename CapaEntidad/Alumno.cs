70351242fba5e16e9eb63db3953f1af72e5ad73d
blacky <72181620@continental.edu.pe>
2024-04-20 21:26:34 -0500

    POO C

---
+++ CapaEntidad/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaEntidad
+{
+    public class Alumno : Persona
+    {
+        private string codigo;
+        private string telefono;
+
+        public string Codigo
+        {
+            get { return this.codigo; }
+            set { this.codigo = value; }
+        }
+
+        public string Telefono
+        {
+            get { return this.telefono; }
+            set { this.telefono = value; }
+        }
+
+        public string DevolverDatosAlumno()
+        {
+            return "Codigo: " + this.codigo + "Apellidos: " + this.Apellidos + "Nombres: " + this.Nombres + "Edad: " + Convert.ToString(this.Edad) + "Telefono: " + this.telefono;
+        }
+
+    }
+}
---
+++ CapaPresentacion/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidad;
+
+namespace CapaPresentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static Persona persona = new Persona();
+
+        private void BtnRegistrar_Click(object sender, EventArgs e)
+        {
+            //Asignar valores a los atributos de la clase persona 
+            persona.Apellidos = txtApellidos.Text;
+            persona.Nombres = txtNombres.Text;
+            persona.Edad = Convert.ToInt32 (txtEdad.Text);
+            MessageBox.Show("Se registro correctamente el objeto Persona");
+        }
+
+        private void BtnReporte_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(persona.DevolverDatos());       
+        }
+    }
+}
